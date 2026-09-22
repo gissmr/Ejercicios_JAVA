653d152b65bc586d6f315420ca9d33d64530c838
Gisescobar <gis.escobar@duocuc.cl>
2026-09-22 05:06:02 -0300

    Métodos finales de lógica e impresión terminados

--- Guia_1.4.3/src/Ejercicio1/ProductoIndustrial.java
+++ Guia_1.4.3/src/Ejercicio1/ProductoIndustrial.java
@@ -7,7 +7,7 @@ public class ProductoIndustrial {
     private double precioUnitario;
     private int cantidadDisponible;
 
-    public ProductoIndustrial(String codigo, String nombre, String categoria, double precioUnitario, int cantidadDisponible) throws IllegalAccessException {
+    public ProductoIndustrial(String codigo, String nombre, String categoria, double precioUnitario, int cantidadDisponible) {
 
         if (codigo.isEmpty()) {
             throw new IllegalArgumentException("El código no puede estar vacío");
@@ -77,4 +77,35 @@ public class ProductoIndustrial {
         return this.nombre.equalsIgnoreCase(texto) || this.categoria.equalsIgnoreCase(texto);
     }
 
+    public void aumentarStock(int cantidad) {
+        if (cantidad <= 0) {
+            throw new IllegalArgumentException("El número debe ser positivo y mayor a 0");
+        }
+        this.cantidadDisponible = this.cantidadDisponible + cantidad;
+    }
+
+    public boolean disminuirStock(int cantidad) {
+        if (cantidad <=0){
+            throw new IllegalArgumentException("La cantidad debe ser mayor a 0");
+        }
+        if (cantidad > this.cantidadDisponible) {
+            throw new IllegalArgumentException("No hay stock suficiente");
+        }
+        this.cantidadDisponible = this.cantidadDisponible - cantidad;
+        return  true;
+    }
+
+    public boolean tieneStockBajo (int limite) {
+        return this.cantidadDisponible < limite;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("--- DATOS DEL PRODUCTO ---");
+        System.out.println("Código: " + this.codigo);
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Categoría: " + this.categoria);
+        System.out.println("Precio Unitario: $" + this.precioUnitario);
+        System.out.println("Stock Disponible: " + this.cantidadDisponible + " unidades");
+        System.out.println("--------------------------");
+    }
 }
